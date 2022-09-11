625bb4ffa7c337b61a4f7cc4e1398738829dbf6e
good_pen <5649701+llwwff@user.noreply.gitee.com>
2022-09-11 12:16:19 +0800

    扔掉test

--- README.md
+++ README.md
@@ -1,12 +1,8 @@
 # nvim-dap-ui
 
-This is still early stage software. Bugs are expected and there may be breaking
-changes!
-
-## Introduction
-
-A UI for [nvim-dap](https://github.com/mfussenegger/nvim-dap) which provides a
-good out of the box configuration.
+This is still early stage software.
+Bugs are expected and
+there may be breaking changes!   
 
 ![Preview](https://user-images.githubusercontent.com/24252670/126842672-de9c6b78-eec2-4187-b48e-977686ec4080.png)
 
@@ -36,78 +32,83 @@ use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} }
 
 ## Configuration
 
-nvim-dap-ui is built on the idea of "elements". These elements are windows
-which provide different features.
+nvim-dap-ui is built on the idea of "elements".
+These elements are windows which
+provide different features.
 
-The UI is split between a sidebar which sits on the side of the screen, and a
-tray which sits on the bottom of the screen by default. Both of these can
-contain any of the available elements and the position of each can be changed
-to any side of the screen.
+
+The UI is split between
+a sidebar which
+sits on the side of the screen,
+and a tray which
+sits on the bottom of the screen by default.
+Both of these can contain any of the available elements and
+    the position of each can be changed to any side of the screen.  
 
 Elements can also be displayed temporarily in a floating window.
 
-You can supply an object to the `require("dapui").setup()` function to
+You can supply an object to the `lua require("dapui").setup()` function to
 configure the elements.
 
-Default settings:
+Default lua settings:
 
 ```lua
-require("dapui").setup({
-  icons = { expanded = "▾", collapsed = "▸" },
-  mappings = {
-    -- Use a table to apply multiple mappings
-    expand = { "<CR>", "<2-LeftMouse>" },
-    open = "o",
-    remove = "d",
-    edit = "e",
-    repl = "r",
-    toggle = "t",
-  },
-  -- Expand lines larger than the window
-  -- Requires >= 0.7
-  expand_lines = vim.fn.has("nvim-0.7"),
-  -- Layouts define sections of the screen to place windows.
-  -- The position can be "left", "right", "top" or "bottom".
-  -- The size specifies the height/width depending on position. It can be an Int
-  -- or a Float. Integer specifies height/width directly (i.e. 20 lines/columns) while
-  -- Float value specifies percentage (i.e. 0.3 - 30% of available lines/columns)
-  -- Elements are the elements shown in the layout (in order).
-  -- Layouts are opened in order so that earlier layouts take priority in window sizing.
-  layouts = {
-    {
-      elements = {
-      -- Elements can be strings or table with id and size keys.
-        { id = "scopes", size = 0.25 },
-        "breakpoints",
-        "stacks",
-        "watches",
+    require("dapui").setup({
+      icons = { expanded = "▾", collapsed = "▸" },
+      mappings = {
+        -- Use a table to apply multiple mappings
+        expand = { "<CR>", "<2-LeftMouse>" },
+        open = "o",
+        remove = "d",
+        edit = "e",
+        repl = "r",
+        toggle = "t",
       },
-      size = 40, -- 40 columns
-      position = "left",
-    },
-    {
-      elements = {
-        "repl",
-        "console",
+      -- Expand lines larger than the window
+      -- Requires >= 0.7
+      expand_lines = vim.fn.has("nvim-0.7"),
+      -- Layouts define sections of the screen to place windows.
+      -- The position can be "left", "right", "top" or "bottom".
+      -- The size specifies the height/width depending on position. It can be an Int
+      -- or a Float. Integer specifies height/width directly (i.e. 20 lines/columns) while
+      -- Float value specifies percentage (i.e. 0.3 - 30% of available lines/columns)
+      -- Elements are the elements shown in the layout (in order).
+      -- Layouts are opened in order so that earlier layouts take priority in window sizing.
+      layouts = {
+        {
+          elements = {
+          -- Elements can be strings or table with id and size keys.
+            { id = "scopes", size = 0.25 },
+            "breakpoints",
+            "stacks",
+            "watches",
+          },
+          size = 40, -- 40 columns
+          position = "left",
+        },
+        {
+          elements = {
+            "repl",
+            "console",
+          },
+          size = 0.25, -- 25% of total lines
+          position = "bottom",
+        },
       },
-      size = 0.25, -- 25% of total lines
-      position = "bottom",
-    },
-  },
-  floating = {
-    max_height = nil, -- These can be integers or a float between 0 and 1.
-    max_width = nil, -- Floats will be treated as percentage of your screen.
-    border = "single", -- Border style. Can be "single", "double" or "rounded"
-    mappings = {
-      close = { "q", "<Esc>" },
-    },
-  },
-  windows = { indent = 1 },
-  render = {
-    max_type_length = nil, -- Can be integer or nil.
-    max_value_lines = 100, -- Can be integer or nil.
-  }
-})
+      floating = {
+        max_height = nil, -- These can be integers or a float between 0 and 1.
+        max_width = nil, -- Floats will be treated as percentage of your screen.
+        border = "single", -- Border style. Can be "single", "double" or "rounded"
+        mappings = {
+          close = { "q", "<Esc>" },
+        },
+      },
+      windows = { indent = 1 },
+      render = {
+        max_type_length = nil, -- Can be integer or nil.
+        max_value_lines = 100, -- Can be integer or nil.
+      }
+    })
 ```
 
 ### Variable Scopes
@@ -143,10 +144,14 @@ Mappings:
 
 Element ID: `watches`
 
-Allows creation of expressions to watch the value of in the context of the
-current frame.
-This uses a prompt buffer for input. To enter a new expression, just enter
-insert mode and you will see a prompt appear. Press enter to submit
+Allows creation of expressions to
+    watch the value of in the context of the current frame.
+This uses a prompt buffer for input.
+To enter a new expression,
+    just enter insert mode and
+    you will see a prompt appear.
+Press enter to submit
+
 
 Mappings:
 
@@ -182,19 +187,20 @@ The console window used by nvim-dap for the integrated terminal.
 
 ## Usage
 
-To get started simply call the setup method on startup, optionally providing
-custom settings.
+To get started simply call the setup method on startup,
+optionally providing  custom settings.
 
 ```lua
-require("dapui").setup()
+    lua require("dapui").setup()
 ```
+
 
 You can open, close and toggle the windows with corresponding functions:
 
 ```lua
-require("dapui").open()
-require("dapui").close()
-require("dapui").toggle()
+    lua require("dapui").open()
+    lua require("dapui").close()
+    lua require("dapui").toggle()
 ```
 
 Each of the functions optionally takes either `"sidebar"` or `"tray"` as an
@@ -217,16 +223,19 @@ end
 
 ### Floating Elements
 
-For elements that are not opened in the tray or sidebar, you can open them in a
-floating window.
+For elements that are not opened in the tray or
+sidebar,
+you can open them in a floating window.
+
 
 ![image](https://user-images.githubusercontent.com/24252670/126844102-8789effb-4276-4599-afe6-a074b019c38d.png)
 
 ```lua
-require("dapui").float_element(<element ID>, <optional settings>)
+lua require("dapui").float_element(<element ID>, <optional settings>)
 ```
 
-If you do not provide an element ID, you will be queried to select one.
+If you do not provide an element ID,
+you will be queried to select one.
 
 The optional settings can included the following keys:
 
@@ -234,28 +243,33 @@ The optional settings can included the following keys:
 - `height: number` Height of the window
 - `enter: boolean` Enter the floating window
 
-Call the same function again while the window is open and the cursor will jump
-to the floating window. The REPL will automatically jump to the floating
-window on open.
+Call the same function again while the window is open and
+the cursor will jump to the floating window.
+The REPL will automatically jump to the floating window on open.
+
 
 ### Evaluate Expression
 
-For a one time expression evaluation, you can call a hover window to show a value
+For a one time expression evaluation,
+you can call a hover window to show a value
 
 ![image](https://user-images.githubusercontent.com/24252670/126844454-691d691c-4550-46fe-89dc-25e1e9681545.png)
 
 ```lua
-require("dapui").eval(<expression>)
+lua require("dapui").eval(<expression>)
 ```
 
-If an expression is not provided it will use the word under the cursor, or if in
-visual mode, the currently highlighted text.
-You can define a visual mapping like so
+If an expression is not provided it will use the word under the cursor,
+or
+if in visual mode,
+    the currently highlighted text.
+You can define a visual mapping like so 
 
 ```vim
-vnoremap <M-k> <Cmd>lua require("dapui").eval()<CR>
+    vnoremap <M-k> <Cmd>lua require("dapui").eval()<CR>
 ```
 
-Call the same function again while the window is open to jump to the eval window.
+Call the same function again while the window is open to
+jump to the eval window.
 
 The same mappings as the variables element apply within the hover window.
--- lua/dapui/init.lua
+++ lua/dapui/init.lua
@@ -7,62 +7,62 @@ local ui_state
 
 ---@return Element
 local function element(name)
-  return require("dapui.elements." .. name)
+    return require("dapui.elements." .. name)
 end
 
 local open_float = nil
 
 local function query_elem_name(on_select)
-  local entries = {}
-  local elems = {}
-  for _, name in pairs(config.elements) do
-    if name ~= config.elements.HOVER then
-      entries[#entries + 1] = name
-      elems[#elems + 1] = name
+    local entries = {}
+    local elems = {}
+    for _, name in pairs(config.elements) do
+        if name ~= config.elements.HOVER then
+            entries[#entries + 1] = name
+            elems[#elems + 1] = name
+        end
     end
-  end
-  vim.ui.select(entries, {
-    prompt = "Select an element:",
-    format_item = function(entry)
-      return entry:sub(1, 1):upper() .. entry:sub(2)
-    end,
-  }, on_select)
+    vim.ui.select(entries, {
+        prompt = "Select an element:",
+        format_item = function(entry)
+            return entry:sub(1, 1):upper() .. entry:sub(2)
+        end,
+    }, on_select)
 end
 
 ---Open a floating window containing the desired element.
 ---
 ---If no fixed dimensions are given, the window will expand to fit the contents
 ---of the buffer.
----@param elem_name string
----@param settings table
----@field width integer: Fixed width of window
----@field height integer: Fixed height of window
----@field enter boolean: Whether or not to enter the window after opening
+---@param  elem_name string
+---@param  settings table
+---@field   width integer: Fixed width of window
+---@field   height integer: Fixed height of window
+---@field   enter boolean: Whether or not to enter the window after opening
 function dapui.float_element(elem_name, settings)
-  vim.schedule(function()
-    if open_float then
-      return open_float:jump_to()
-    end
-    local line_no = vim.fn.screenrow()
-    local col_no = vim.fn.screencol()
-    local position = { line = line_no, col = col_no }
-    local with_elem = vim.schedule_wrap(function(elem_name)
-      if not elem_name then
-        return
-      end
-      local elem = element(elem_name)
-      local settings = vim.tbl_deep_extend("keep", settings or {}, elem.float_defaults or {})
-      open_float = require("dapui.windows").open_float(elem, position, settings)
-      open_float:listen("close", function()
-        open_float = nil
-      end)
+    vim.schedule(function()
+        if open_float then
+            return open_float:jump_to()
+        end
+        local line_no = vim.fn.screenrow()
+        local col_no = vim.fn.screencol()
+        local position = { line = line_no, col = col_no }
+        local with_elem = vim.schedule_wrap(function(elem_name)
+            if not elem_name then
+                return
+            end
+            local elem = element(elem_name)
+            local settings = vim.tbl_deep_extend("keep", settings or {}, elem.float_defaults or {})
+            open_float = require("dapui.windows").open_float(elem, position, settings)
+            open_float:listen("close", function()
+                open_float = nil
+            end)
+        end)
+        if elem_name then
+            with_elem(elem_name)
+            return
+        end
+        query_elem_name(with_elem)
     end)
-    if elem_name then
-      with_elem(elem_name)
-      return
-    end
-    query_elem_name(with_elem)
-  end)
 end
 
 ---Open a floating window containing the result of evaluting an expression
@@ -76,115 +76,115 @@ end
 ---@field height integer: Fixed height of window
 ---@field enter boolean: Whether or not to enter the window after opening
 function dapui.eval(expr, settings)
-  settings = settings or {}
-  if open_float then
-    open_float:jump_to()
-    return
-  end
-  if not expr then
-    if vim.fn.mode() == "v" then
-      local start = vim.fn.getpos("v")
-      local finish = vim.fn.getpos(".")
-      local lines = require("dapui.util").get_selection(start, finish)
-      expr = table.concat(lines, "\n")
-    else
-      expr = expr or vim.fn.expand("<cexpr>")
+    settings = settings or {}
+    if open_float then
+        open_float:jump_to()
+        return
     end
-  end
-  local elem = require("dapui.elements.hover")
-  elem.set_expression(expr, settings.context)
-  vim.schedule(function()
-    local line_no = vim.fn.screenrow()
-    local col_no = vim.fn.screencol()
-    local position = { line = line_no, col = col_no }
-    open_float = require("dapui.windows").open_float(elem, position, settings)
-    open_float:listen("close", function()
-      open_float = nil
+    if not expr then
+        if vim.fn.mode() == "v" then
+            local start = vim.fn.getpos("v")
+            local finish = vim.fn.getpos(".")
+            local lines = require("dapui.util").get_selection(start, finish)
+            expr = table.concat(lines, "\n")
+        else
+            expr = expr or vim.fn.expand("<cexpr>")
+        end
+    end
+    local elem = require("dapui.elements.hover")
+    elem.set_expression(expr, settings.context)
+    vim.schedule(function()
+        local line_no = vim.fn.screenrow()
+        local col_no = vim.fn.screencol()
+        local position = { line = line_no, col = col_no }
+        open_float = require("dapui.windows").open_float(elem, position, settings)
+        open_float:listen("close", function()
+            open_float = nil
+        end)
     end)
-  end)
 end
 
 function dapui.setup(user_config)
-  local dap = require("dap")
-  local render = require("dapui.render")
-  if ui_state then
-    vim.notify("Setup called twice", "warn", {
-      title = "nvim-dap-ui",
-      icon = " ",
-    })
-  end
-  render.loop.clear()
-
-  config.setup(user_config)
-
-  local UIState = require("dapui.state")
-  ui_state = UIState()
-  ui_state:attach(dap)
-
-  for _, module in pairs(config.elements) do
-    local elem = element(module)
-    elem.setup(ui_state)
-    render.loop.register_element(elem)
-    for _, event in pairs(elem.dap_after_listeners or {}) do
-      dap.listeners.after[event]["DapUI " .. elem.name] = function()
-        render.loop.run(elem.name)
-      end
+    local dap = require("dap")
+    local render = require("dapui.render")
+    if ui_state then
+        vim.notify("Setup called twice", "warn", {
+            title = "nvim-dap-ui",
+            icon = " ",
+        })
     end
-  end
+    render.loop.clear()
 
-  windows.setup()
+    config.setup(user_config)
 
-  ui_state:on_refresh(function()
-    render.loop.run()
-  end)
-  ui_state:on_clear(function()
-    render.loop.run()
-  end)
+    local UIState = require("dapui.state")
+    ui_state = UIState()
+    ui_state:attach(dap)
+
+    for _, module in pairs(config.elements) do
+        local elem = element(module)
+        elem.setup(ui_state)
+        render.loop.register_element(elem)
+        for _, event in pairs(elem.dap_after_listeners or {}) do
+            dap.listeners.after[event]["DapUI " .. elem.name] = function()
+                render.loop.run(elem.name)
+            end
+        end
+    end
+
+    windows.setup()
+
+    ui_state:on_refresh(function()
+        render.loop.run()
+    end)
+    ui_state:on_clear(function()
+        render.loop.run()
+    end)
 end
 
 local function keep_cmdheight(cb)
-  local cmd_height = vim.o.cmdheight
+    local cmd_height = vim.o.cmdheight
 
-  cb()
+    cb()
 
-  vim.o.cmdheight = cmd_height
+    vim.o.cmdheight = cmd_height
 end
 
 ---Close one or all of the window layouts
 ---@param opts table
 ---@field layout number|nil: Index of layout in config
 function dapui.close(opts)
-  keep_cmdheight(function()
-    opts = opts or {}
-    if type(opts) == "number" then
-      opts = { layout = opts }
-    end
-    local layout = opts.layout
+    keep_cmdheight(function()
+        opts = opts or {}
+        if type(opts) == "number" then
+            opts = { layout = opts }
+        end
+        local layout = opts.layout
 
-    for _, win_layout in ipairs(windows.layouts) do
-      win_layout:update_sizes()
-    end
-    for i, win_layout in ipairs(windows.layouts) do
-      if not layout or layout == i then
-        win_layout:update_sizes()
-        win_layout:close()
-      end
-    end
-  end)
+        for _, win_layout in ipairs(windows.layouts) do
+            win_layout:update_sizes()
+        end
+        for i, win_layout in ipairs(windows.layouts) do
+            if not layout or layout == i then
+                win_layout:update_sizes()
+                win_layout:close()
+            end
+        end
+    end)
 end
 
 ---@generic T
 ---@param list T[]
 ---@return fun(): number, T
 local function reverse(list)
-  local i = #list + 1
-  return function()
-    i = i - 1
-    if i <= 0 then
-      return nil
+    local i = #list + 1
+    return function()
+        i = i - 1
+        if i <= 0 then
+            return nil
+        end
+        return i, list[i]
     end
-    return i, list[i]
-  end
 end
 
 ---Open one or all of the window layouts
@@ -192,42 +192,42 @@ end
 ---@field layout number|nil: Index of layout in config
 ---@field reset boolean: Reset windows to original size
 function dapui.open(opts)
-  keep_cmdheight(function()
-    opts = opts or {}
-    if type(opts) == "number" then
-      opts = { layout = opts }
-    end
-    local layout = opts.layout
-
-    for _, win_layout in ipairs(windows.layouts) do
-      win_layout:update_sizes()
-    end
-    local closed = {}
-    if layout then
-      for i = 1, (layout and layout - 1) or #windows.layouts, 1 do
-        if windows.layouts[i]:is_open() then
-          closed[#closed + 1] = i
-          windows.layouts[i]:close()
+    keep_cmdheight(function()
+        opts = opts or {}
+        if type(opts) == "number" then
+            opts = { layout = opts }
         end
-      end
-    end
+        local layout = opts.layout
 
-    for i, win_layout in reverse(windows.layouts) do
-      if not layout or layout == i then
-        win_layout:open()
-      end
-    end
+        for _, win_layout in ipairs(windows.layouts) do
+            win_layout:update_sizes()
+        end
+        local closed = {}
+        if layout then
+            for i = 1, (layout and layout - 1) or #windows.layouts, 1 do
+                if windows.layouts[i]:is_open() then
+                    closed[#closed + 1] = i
+                    windows.layouts[i]:close()
+                end
+            end
+        end
 
-    if #closed > 0 then
-      for _, i in ipairs(closed) do
-        windows.layouts[i]:open()
-      end
-    end
+        for i, win_layout in reverse(windows.layouts) do
+            if not layout or layout == i then
+                win_layout:open()
+            end
+        end
 
-    for _, win_layout in ipairs(windows.layouts) do
-      win_layout:resize(opts)
-    end
-  end)
+        if #closed > 0 then
+            for _, i in ipairs(closed) do
+                windows.layouts[i]:open()
+            end
+        end
+
+        for _, win_layout in ipairs(windows.layouts) do
+            win_layout:resize(opts)
+        end
+    end)
 end
 
 ---Toggle one or all of the window layouts.
@@ -235,41 +235,41 @@ end
 ---@field layout number|nil: Index of layout in config
 ---@field reset boolean: Reset windows to original size
 function dapui.toggle(opts)
-  keep_cmdheight(function()
-    opts = opts or {}
-    if type(opts) == "number" then
-      opts = { layout = opts }
-    end
-    local layout = opts.layout
-
-    for _, win_layout in reverse(windows.layouts) do
-      win_layout:update_sizes()
-    end
-
-    local closed = {}
-    if layout then
-      for i = 1, (layout and layout - 1) or #windows.layouts, 1 do
-        if windows.layouts[i]:is_open() then
-          closed[#closed + 1] = i
-          windows.layouts[i]:close()
+    keep_cmdheight(function()
+        opts = opts or {}
+        if type(opts) == "number" then
+            opts = { layout = opts }
         end
-      end
-    end
+        local layout = opts.layout
 
-    for i, win_layout in reverse(windows.layouts) do
-      if not layout or layout == i then
-        win_layout:toggle()
-      end
-    end
+        for _, win_layout in reverse(windows.layouts) do
+            win_layout:update_sizes()
+        end
 
-    for _, i in reverse(closed) do
-      windows.layouts[i]:open()
-    end
+        local closed = {}
+        if layout then
+            for i = 1, (layout and layout - 1) or #windows.layouts, 1 do
+                if windows.layouts[i]:is_open() then
+                    closed[#closed + 1] = i
+                    windows.layouts[i]:close()
+                end
+            end
+        end
 
-    for _, win_layout in ipairs(windows.layouts) do
-      win_layout:resize(opts)
-    end
-  end)
+        for i, win_layout in reverse(windows.layouts) do
+            if not layout or layout == i then
+                win_layout:toggle()
+            end
+        end
+
+        for _, i in reverse(closed) do
+            windows.layouts[i]:open()
+        end
+
+        for _, win_layout in ipairs(windows.layouts) do
+            win_layout:resize(opts)
+        end
+    end)
 end
 
 return dapui
